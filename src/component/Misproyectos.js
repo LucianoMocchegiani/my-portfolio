eacc95ee5e455a679095b20ec76b3c8fe13a1c0c
LucianoMocchegiani <lucianomocchegiani@hotmail.com.ar>
2023-05-13 20:53:34 -0300

    ss

--- src/component/Misproyectos.js
+++ src/component/Misproyectos.js
@@ -1,13 +1,17 @@
 import react,{useState} from 'react'
 import './Misproyectos.css';
+import imagen3 from '../business-admin-imagen.png'
+import imagen2 from '../timberli-project.png'
+import imagen1 from '../henry-food.png'
 
 export default function Misproyectos(){
     
-    const imagenesProyectos=["https://camo.githubusercontent.com/6558936db11734287b508089ba329beb8dafbfeac498a993f582a5edd2a5874b/68747470733a2f2f692e706f7374696d672e63632f72704c76594d6e4b2f436170747572612d64652d70616e74616c6c612d64652d323032322d30362d31312d31392d34352d30342e706e67",
-    "https://camo.githubusercontent.com/e86680d6cb82db6667a045c9601a31dc749a00ea585841101b4b65fc14c34754/68747470733a2f2f692e706f7374696d672e63632f4331344b445971592f436170747572612d64652d70616e74616c6c612d64652d323032322d30372d30342d32302d31332d32372e706e67"]
-    const linksProyectos=["https://client-food-project.herokuapp.com/home","https://proyecto-final-orcin-seven.vercel.app/"]
-    const resumenesProyectos=["HenryFood:   Este fue un proyecto individual del bootcamp Henry hice esta app con las tecnologías React Redux en el frontend y Node.js, Express.js y Sequelize en el backend. es una app donde se crean y se visualizan recetas de comidas.",
-    "Timberli:   Hice esta app en conjunto con un grupo de compañeros utilizamos git  usamos las tecnologías React Redux en el frontend y Node.js, Express.js y Sequelize en el backend. Tiene autenticación (Oauth2.0), Método de pago implementado con STRIPE. la idea de la app es ser una red social donde software developers Jr puedan subir y exponer su portfolio de proyectos, para mostrarse al mundo y recibir feedback."]
+    const imagenesProyectos=[imagen1,
+    imagen2,imagen3]
+    const linksProyectos=["https://pi-food-yqzu8pfxg-timberli.vercel.app/","https://proyecto-final-orcin-seven.vercel.app/",'https://businessadmin.vercel.app/']
+    const resumenesProyectos=["HenryFood: Tecnologías React Redux en el frontend y Node.js, Express.js y Sequelize en el backend. es una app donde se crean y se visualizan recetas de comidas.",
+    "Timberli:   Hice esta app en conjunto con un grupo de compañeros utilizamos git  usamos las tecnologías React Redux en el frontend y Node.js, Express.js y Sequelize en el backend. Tiene autenticación (Oauth2.0), Método de pago implementado con STRIPE. la idea de la app es ser una red social donde software developers Jr puedan subir y exponer su portfolio de proyectos, para mostrarse al mundo y recibir feedback.",
+    "Busines Admin: Web y App mobile para gestionar negocios comerciales."]
     const [carrusel, setCarrusel] = useState(0);
     const carruselProyectos=(valor)=>{
       if(valor==">"&&carrusel==imagenesProyectos.length-1){
