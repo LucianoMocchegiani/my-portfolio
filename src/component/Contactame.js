d0373a9c06f4429f356998cc482ac9ed23ae311b
LucianoMocchegiani <lucianomocchegiani@hotmail.com.ar>
2023-05-21 21:57:16 -0300

    SS

--- src/component/Contactame.js
+++ src/component/Contactame.js
@@ -1,28 +1,35 @@
 
 import './Contactame.css';
-import emailjs from 'emailjs-com'
+import React, { useRef } from 'react';
+import emailjs from '@emailjs/browser';
 
 export default function Contactame(){
     window.scroll(0,0)
+    const form = useRef();
 
-    const enviarMail = (e)=>{       
+    const sendEmail = (e) => {
         e.preventDefault();
-        emailjs.sendForm('service_50e21pu','template_fc3ok68',e.target,'0o8mcv869UgSykydv')
-        .then(res=>{
-          alert('se a enviado correctamente.');
-          console.log(res);
-        })
+
+        emailjs.sendForm('service_7vp5zlj', 'template_0vl1hsg', form.current, '_ZfQzwicAJcI7E1ev')
+        .then((result) => {
+            console.log(result.text);
+        }, (error) => {
+            console.log(error.text);
+        }).then(
+            console.log(form.current)
+        )
+
     }
 
     return(     
         <div className="divContactame">
             <div className='contactameId'id='contactame'></div>
             <p className='contactameTitle' >Gracias por tomarse el tiempo para comunicarse. ¿Cómo puedo ayudarte?</p>   
-            <form onSubmit={enviarMail} className='form'> 
-                <input type='text'className='entradasCorreo1'id='email'name='email'placeholder='Correo electronico...'/>
-                <input type='text'className='entradasCorreo2'id='email'name='nombre'placeholder='Su nombre...'/>
-                <input type='text'className='entradasCorreo3'id='mensaje'name='mensaje'placeholder='Escriba su mensaje...'/>     
-                <button type='submit'className='enviar'>Enviar</button>   
+            <form ref={form} onSubmit={sendEmail} className='form'> 
+                <input type='email'className='entradasCorreo1'id='email'name="user_email"placeholder='Correo electronico...'/>
+                <input type='text'className='entradasCorreo2'id='email'name="user_name"placeholder='Su nombre...'/>
+                <input type='text'className='entradasCorreo3'id='mensaje'name="message"placeholder='Escriba su mensaje...'/>     
+                <input type='submit'className='enviar' value='Enviar'/>   
             </form>
         </div>
     )
