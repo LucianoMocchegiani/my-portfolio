0984a18e5e6fa6c2a5c1836ec6506212804f97e3
LucianoMocchegiani <lucianomocchegiani@hotmail.com.ar>
2023-05-22 21:00:35 -0300

    SS

--- src/component/Contactame.js
+++ src/component/Contactame.js
@@ -1,24 +1,75 @@
-
+import {useState} from 'react'
 import './Contactame.css';
 import React, { useRef } from 'react';
 import emailjs from '@emailjs/browser';
+import { useNavigate } from 'react-router';
+import {alertConfirmacion} from '../functions/Alert'
 
+
+function validate(input) {
+    let errors = {};
+    if (!input.user_email) {
+      errors.user_email = '*ingresa un email'
+    // }else if (emailRegex.test(input.email)){
+    //   errors.email = '*formato incorrecto'
+    }
+    if (!input.user_name){
+      errors.user_name = '*ingresa un nombre'
+    }else if (input.user_name.length > 80){
+      errors.user_name = '*no puede contener mas de 80 caracteres'
+    }
+    if(!input.message){
+      errors.message = '*ingresa un mensaje'
+    }else if (input.message.length > 1000){
+        errors.message = '*no puede contener mas de 1000 caracteres'
+    }
+    
+    return errors;
+  };
 export default function Contactame(){
+    const navigate = useNavigate()
     window.scroll(0,0)
     const form = useRef();
 
+    const [datos,setDatos] = useState({
+        user_email:'',
+        user_name:'',
+        message:''
+    })
+    const handleChangeText = (e) => {
+        setDatos({ ...datos, [e.target.name]: e.target.value });
+    };
+    const [formErrors, setFormErrors] = useState(false)
+  
+    const emailjsSend = async ()=>{
+        await emailjs.sendForm('service_7vp5zlj', 'template_0vl1hsg', form.current, '_ZfQzwicAJcI7E1ev')
+            .then((result) => {
+                console.log(result.text);
+            }, (error) => {
+                console.log(error.text);
+            }).then(
+                console.log(form.current)
+            )
+        setDatos(
+            {user_email:'',
+            user_name:'',
+            message:''})
+        // navigate('/')
+        return true
+    }
+
     const sendEmail = (e) => {
         e.preventDefault();
+        // setFormErrors(true)
+        if(Object.keys(validate(datos)).length){
+            console.log(validate(datos))
+            setFormErrors(true)
+        }
+        else{
 
-        emailjs.sendForm('service_7vp5zlj', 'template_0vl1hsg', form.current, '_ZfQzwicAJcI7E1ev')
-        .then((result) => {
-            console.log(result.text);
-        }, (error) => {
-            console.log(error.text);
-        }).then(
-            console.log(form.current)
-        )
-
+            alertConfirmacion("Enviar Mensaje?",null,emailjsSend)
+            setFormErrors(false)
+        }
     }
 
     return(     
@@ -26,10 +77,19 @@ export default function Contactame(){
             <div className='contactameId'id='contactame'></div>
             <p className='contactameTitle' >Gracias por tomarse el tiempo para comunicarse. ¿Cómo puedo ayudarte?</p>   
             <form ref={form} onSubmit={sendEmail} className='form'> 
-                <input type='email'className='entradasCorreo1'id='email'name="user_email"placeholder='Correo electronico...'/>
-                <input type='text'className='entradasCorreo2'id='email'name="user_name"placeholder='Su nombre...'/>
-                <input type='text'className='entradasCorreo3'id='mensaje'name="message"placeholder='Escriba su mensaje...'/>     
-                <input type='submit'className='enviar' value='Enviar'/>   
+                <div className='entradasCorreo1Div'>
+                    <input onChange={handleChangeText} value={datos.user_email} type='email'className='entradasCorreo1'id='email'name="user_email"placeholder='Correo electronico...'/>
+                    <div className='redError-container'>{validate(datos).user_email&&formErrors&&<p className='redError'>{validate(datos).user_email}</p>}</div>
+                </div>
+                <div className='entradasCorreo2Div'>
+                    <input onChange={handleChangeText} value={datos.user_name} type='text'className='entradasCorreo2'id='name'name="user_name"placeholder='Su nombre...'/>
+                    <div className='redError-container'>{validate(datos).user_name&&formErrors&&<p className='redError'>{validate(datos).user_name}</p>}</div>
+                </div>
+                <div className='entradasCorreo3Div'>
+                    <input onChange={handleChangeText} value={datos.message} type='text'className='entradasCorreo3'id='message'name="message"placeholder='Escriba su mensaje...'/> 
+                    <div className='redError-container'>{validate(datos).message&&formErrors&&<p className='redError'>{validate(datos).message}</p>}</div>
+                </div>
+                <input  type='submit'className='enviar' value='Enviar'/>   
             </form>
         </div>
     )
---
+++ src/functions/Alert.js
@@ -0,0 +1,49 @@
+import swal from "sweetalert";
+
+
+function alertCustom2(text){
+    
+    swal({
+        title:text,
+        text:text,
+        icon:"success",
+        // icon:"info",
+        // icon:"error",
+        // icon:"warning",
+        // buttons: "Aceptar",
+        buttons:["No","Si"],
+        // timer:"2000",
+    })
+}
+export  function alertConfirmacion(pregunta,text,functionAlert,textError){
+    
+    swal({
+        title:pregunta,
+        text:text,
+        icon:"warning",
+        buttons:["No","Si"],
+    })
+    .then(res=>{
+        if(res){
+            console.log('.......................')
+            console.log(res)
+            if(functionAlert){
+                if(functionAlert()){
+                    swal({
+                        icon:"success",
+                        timer:"7000",
+                    })
+    
+                }else{
+                    swal({
+                        text:textError?textError:'Accion Invalida',
+                        icon:'error',
+                        timer:"4000",
+                    })
+                }
+            }
+            
+           
+        }
+    })
+}
