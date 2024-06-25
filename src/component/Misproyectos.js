d278ae584d3e00b7a85aac1bca8ed0a15aeeb7fd
Luciano Mocchegiani <63886266+LucianoMocchegiani@users.noreply.github.com>
2024-06-25 06:47:36 -0300

    Update Misproyectos.js

--- src/component/Misproyectos.js
+++ src/component/Misproyectos.js
@@ -13,8 +13,8 @@ import {NextArrow, PrevArrow} from './CustomArrowsSlider';
 export default function Misproyectos(){
   const [onHover,setOnHover]= useState({on:false, in:null})
     const objProyect=[
-       {name:'Movies&SeriesApp',link:"https://expo.dev/artifacts/eas/c63mXaD4p77QbUfT8xdRPs.apk", imagen:imagen4, resumen: "Movies and series app:app developed with react native, expo, firebase."},
-       {name:'OrderStoreApp',link:"https://expo.dev/artifacts/eas/jGKgozWyeyWPLzwadd8gYM.apk", imagen:imagen5, resumen: "App to make and manage purchase orders."},
+       {name:'Movies&SeriesApp',link:"https://expo.dev/artifacts/eas/c63mXaD4p77QbUfT8xdRPs.apk", imagen:imagen4, resumen: "Movies and series app:app developed with react native, expo, firebase.", mobile:true},
+       {name:'OrderStoreApp',link:"https://expo.dev/artifacts/eas/jGKgozWyeyWPLzwadd8gYM.apk", imagen:imagen5, resumen: "App to make and manage purchase orders.",  mobile:true},
       {name:'Timberli',link:"https://proyecto-final-orcin-seven.vercel.app/", imagen:imagen2, resumen: "Timberli:   It is a social network where software developers upload and expose their portfolio of projects. The technologies used: React Redux on the frontend and Node.js, Express.js and Sequelize on the backend. Account with authentication (Oauth2.0), Payment method implemented with STRIPE. "}
       ,{name:'BusinesAdmin',link:'https://business-admin.vercel.app/',imagen:imagen3,resumen:"Busines Admin:   Web and mobile app to manage commercial businesses, the main technologies applied are React, React Native with Expo and Firebase. "},
       {name:'Henryfood',link:"https://pi-food-yqzu8pfxg-timberli.vercel.app/",imagen:imagen1,resumen:"HenryFood:   It is a web application where food recipes are created and displayed. The technologies used: React Redux on the frontend and Node.js, Express.js and Sequelize on the backend."}]
@@ -66,7 +66,7 @@ export default function Misproyectos(){
         <Slider {...settings}>
         {objProyect.map((item)=>(
           <a className='card' key={item.name+'1'} href={item.link} target="_blank" onMouseEnter={()=>setOnHover({on:true, in:item.name+'1'})} onMouseLeave={()=>setOnHover({on:false, in:null})}>
-            {onHover.on&&<p key={item.name+'1'} className={onHover.in===item.name+'1'?'visitar':'displayNone'}>Visitar sitio web</p>}
+            {onHover.on&&<p key={item.name+'1'} className={onHover.in===item.name+'1'?'visitar':'displayNone'}>{item.mobile?'Dowload Apk for android':'Visit web site'}</p>}
             <div className='card-top'>
               <img src={item.imagen} alt={item.name} width={'500px'} height={'250'}/>
               
