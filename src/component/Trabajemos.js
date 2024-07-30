d6f1290c9a5d07afc85c6ca8f91c53b7cd71b30e
Luciano Mocchegiani <63886266+LucianoMocchegiani@users.noreply.github.com>
2024-07-30 10:23:33 -0300

    Update Trabajemos.js

--- src/component/Trabajemos.js
+++ src/component/Trabajemos.js
@@ -17,6 +17,7 @@ export default function Trabajemos(){
     return(     
         <div className={scrollY>=value?'trabajar-juntos-container aparecer2Animation':'trabajar-juntos-container'}>
             <p className='start-project'>Are you interested in working with me?</p>
+            //<a href="https://wa.me/1127126514" target="_blank"><img className='icon5 footerIcon' src={whatsapp} alt="whatsapp"/></a>
             <button className='hagamoslo' onClick={()=>navigate('contactame')}><Icon path={mdiHandshakeOutline} size={1} color={'white'}/><p>let's do it</p></button>   
         </div>
     )
