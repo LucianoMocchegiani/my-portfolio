2b63b899b5045a24b3dccac63ded8432ece79170
Luciano Mocchegiani <63886266+LucianoMocchegiani@users.noreply.github.com>
2024-08-09 10:31:20 -0300

    Create CvDowload.js

---
+++ src/component/CvDowload.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const DownloadCV = () => {
+  return (
+    <div>
+      <h2>Download My CV</h2>
+      <a href="/Luciano Mocchegiani cv 2024.pdf" download="Luciano Mocchegiani cv 2024.pdf">
+        Download CV
+      </a>
+    </div>
+  );
+};
+
+export default DownloadCV;
