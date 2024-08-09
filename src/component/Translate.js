d8323cc649b9e988f1dfe7fe3eaf8f98111a787a
Luciano Mocchegiani <63886266+LucianoMocchegiani@users.noreply.github.com>
2024-08-09 12:26:44 -0300

    Update Translate.js

--- src/component/Translate.js
+++ src/component/Translate.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './Navbar.css';
 
-const TranslateButtons = ({clicked}) => {
+const TranslateButtons = ({ clicked }) => {
   const handleTranslate = (lang) => {
     // Abre la página traducida en una nueva pestaña
     const url = `https://translate.google.com/translate?hl=${lang}&sl=auto&tl=${lang}&u=${encodeURIComponent(window.location.href)}`;
@@ -14,22 +14,22 @@ const TranslateButtons = ({clicked}) => {
   };
 
   return (
-        <>
-          <button 
-            className={clicked ? 'button-navBar' : 'buttonOculto-navBar'}
-            style={{
-              display: 'flex',
-              flexDirection: 'row',
-              alignItems: 'center'
-            }}
-              onClick={() => handleTranslate('es')}>
-             <p className='textButton-Navbar'>Spanish</p>/button>
-          <button 
-              onClick={handleReset}>
-              <p className='textButton-Navbar'>English</p>
-          </button>
-      </>
-
+    <>
+      <button 
+        className={clicked ? 'button-navBar' : 'buttonOculto-navBar'}
+        style={{
+          display: 'flex',
+          flexDirection: 'row',
+          alignItems: 'center'
+        }}
+        onClick={() => handleTranslate('es')}>
+        <p className='textButton-Navbar'>Spanish</p>
+      </button>
+      <button 
+        onClick={handleReset}>
+        <p className='textButton-Navbar'>English</p>
+      </button>
+    </>
   );
 };
 
