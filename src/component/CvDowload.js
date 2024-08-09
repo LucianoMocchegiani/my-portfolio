fa1259f8e32bc096588181b7270986ebd4972d1e
Luciano Mocchegiani <63886266+LucianoMocchegiani@users.noreply.github.com>
2024-08-09 10:37:35 -0300

    Update CvDowload.js

--- src/component/CvDowload.js
+++ src/component/CvDowload.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import Icon from '@mdi/react';
 import { mdiFilePdfBox } from '@mdi/js';
+import './Navbar.css';
 
 const DownloadCV = () => {
   return (
     <div>
       <a href="/Luciano Mocchegiani cv 2024.pdf" download="Luciano Mocchegiani cv 2024.pdf">
-        <h2>Download My CV</h2><Icon path={mdiFilePdfBox} size={1} />
+        <p className='textButton-Navbar'>Download My CV<p><Icon path={mdiFilePdfBox} size={1} />
       </a>
     </div>
   );
